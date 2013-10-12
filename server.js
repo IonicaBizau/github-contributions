ec275a3d548ddb8ded18a53e9296244b0a0db9dd
Ionică Bizău <bizauionica@yahoo.com>
2013-10-12 21:15:24 +0300

    Added functions to get form data and send response.

--- server.js
+++ server.js
@@ -31,8 +31,18 @@ http.createServer(function(req, res) {
     }
 
     if (req.url === "/get-zip") {
-        res.writeHead(200, {"Content-Type": "application/json"});
-        res.end("{}");
+        getFormData(req, res, function (err, data) {
+
+            if (err) { return sendResponse(req, res, err, 400); }
+
+            Contributions.getRepo({
+
+            }, function (err, repoData) {
+                if (err) { return sendResponse(req, res, err, 400); }
+                sendResponse(req, res, repoData);
+            });
+
+        });
         return;
     }
 
@@ -48,3 +58,92 @@ http.createServer(function(req, res) {
     });
 }).listen(9000);
 console.log("Listening on 9000");
+
+/*
+ *  Returns the form data
+ *
+ * */
+function getFormData(request, response, callback) {
+
+    if (typeof response === "function") {
+        callback = response;
+    }
+
+    // the request method must be 'POST'
+    if (request.method == 'POST') {
+
+        var body = '';
+
+        // on data
+        request.on('data', function (data) {
+            // add data to body
+            body += data;
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6) {
+                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                request.connection.destroy();
+            }
+        });
+
+        // and on end
+        request.on('end', function () {
+            // parse body
+            var POST = qs.parse(body);
+            // and callback it!
+            callback(undefined, POST);
+        });
+        return;
+    }
+
+    // if it's  a get request, maybe we have some query data
+    var queryData = Url.parse(request.url, true).query;
+
+    // if not post, callback undefined
+    callback(null, queryData);
+}
+
+/*
+ *  Send response to the client
+ * */
+function sendResponse (req, res, content, status, contentType, force) {
+
+    // if not provided, status is 200
+    status = status || 200;
+
+    // set the content type
+    contentType = contentType || "application/json";
+
+    // set headers and status code
+    res.writeHead(status, {"Content-Type": contentType});
+
+    // handle mongo errors
+    if (content instanceof Error) {
+        content = content.message;
+    }
+
+    // build response
+    var response = (content || {}).constructor === Object ? content : {};
+
+    // if content is string
+    if (typeof content === "string") {
+        // set output
+        response.output = content;
+        // but if it is an error
+        if (status !== 200) {
+            // set error
+            response.error = content;
+            // and delete output
+            delete response.output;
+        }
+    }
+
+    // if we force the response data
+    if (force) {
+        // force it!
+        res.end(content);
+        return;
+    }
+
+    // and end the response
+    res.end(JSON.stringify(response, null, 4));
+};
