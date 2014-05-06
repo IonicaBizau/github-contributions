2bd351ab25a401b1677fa05bd81f0f4ac1b51dbc
Ionică Bizău <bizauionica@yahoo.com>
2014-05-06 17:07:28 +0300

    Add a final commit when finishing

--- bin/toZip.sh
+++ bin/toZip.sh
@@ -1,1 +1,12 @@
+# final commit
+cd $1
+cat <<EOF >README.md
+My GitHub Contributions Calendar
+================================
+This repository was generated with [GitHub Contributions](https://github.com/IonicaBizau/github-contributions) generator. Thanks, @IonicaBizau.
+EOF
+git add .
+git commit -m 'Added title and description'
+
+# now, zip the file
 zip -r $1.zip $1
