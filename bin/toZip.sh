7979f119dc6b31f205cc3dedb97f050201729213
Ionică Bizău <bizauionica@yahoo.com>
2014-05-07 08:29:03 +0300

    Link to my profile

--- bin/toZip.sh
+++ bin/toZip.sh
@@ -3,8 +3,9 @@ cd $1
 cat <<EOF >README.md
 My GitHub Contributions Calendar
 ================================
-This repository was generated with [GitHub Contributions](https://github.com/IonicaBizau/github-contributions) generator. Thanks, @IonicaBizau.
+This repository was generated with [GitHub Contributions](https://github.com/IonicaBizau/github-contributions) generator. Thanks, [@IonicaBizau](https://github.com/IonicaBizau).
 EOF
+
 git add .
 git commit -m 'Added title and description'
 
