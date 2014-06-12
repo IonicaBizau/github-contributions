05201540173aada59f68e45796f9fc143f3835f8
GuilhermeSad <gorgulhoguilherme@gmail.com>
2014-06-12 14:37:24 -0300

    Updating repo name from gh-contributions to github-contributions

--- installer.sh
+++ installer.sh
@@ -1,19 +1,19 @@
 echo "Welcome to Github contributions!"
 sleep 2
-echo "Making gh-contributions directory in ~/"
-rm -rf ~/gh-contributions
-mkdir ~/gh-contributions
-echo "... entering ~/gh-contributions"
-cd ~/gh-contributions
+echo "Making github-contributions directory in ~/"
+rm -rf ~/github-contributions
+mkdir ~/github-contributions
+echo "... entering ~/github-contributions"
+cd ~/github-contributions
 
 echo "... downloading"
 # If wget is not present, use curl.
-wget https://github.com/IonicaBizau/gh-contributions/archive/master.zip || curl -OL https://github.com/IonicaBizau/gh-contributions/archive/master.zip
+wget https://github.com/IonicaBizau/github-contributions/archive/master.zip || curl -OL https://github.com/IonicaBizau/github-contributions/archive/master.zip
 
 echo "... unzipping"
 unzip master.zip
 mv github-contributions-master/* ./
 npm install
 rm -rf github-contributions-master master.zip
-echo "Sucessfully installed. Please type: cd ~/gh-contributions"
+echo "Sucessfully installed. Please type: cd ~/github-contributions"
 echo "Bye!"
