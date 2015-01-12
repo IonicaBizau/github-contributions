7f83a86e70207b1fa4b7567a6789371be2d61005
Ionică Bizău <bizauionica@yahoo.com>
2015-01-12 09:40:56 +0200

    Replaced statique with Lien.

--- server.js
+++ server.js
@@ -1,120 +1,24 @@
-// dependencies
-var Statique = require ("statique")
-  , http = require('http')
-  , qs   = require('querystring')
-  , Contributions = require("./contributions")
+// Dependencies
+var Contributions = require("./contributions")
+  , Lien = require("lien");
+  , SocketIO = require("socket.io")
   ;
 
-// set static server: public folder
-var ghc_server = new Statique({root: __dirname + "/public"})
-ghc_server.setRoutes({"/": "/html/index.html"});
+// Init lien server
+var server = new Lien({
+    host: "localhost"
+  , port: 9000
+});
 
-/*
- *  Returns the form data
- *
- * */
-function getFormData(request, response, callback) {
+// Add page
+server.page.add("/", function (lien) {
+    lien.file("/html/index.html");
+});
 
-    // handle response as function
-    if (typeof response === "function") {
-        callback = response;
-        response = undefined;
-    }
-
-    // the request method must be 'POST'
-    if (request.method == 'POST') {
-
-        var body = '';
-
-        // on data
-        request.on('data', function (data) {
-            // add data to body
-            body += data;
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1e6) {
-                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-                request.connection.destroy();
-            }
-        });
-
-        // and on end
-        request.on('end', function () {
-            // parse body
-            var POST = qs.parse(body);
-            // and callback it!
-            callback(undefined, POST);
-        });
-        return;
-    }
-
-    // if it's  a get request, maybe we have some query data
-    var queryData = Url.parse(request.url, true).query;
-
-    // if not post, callback undefined
-    callback(null, queryData);
-}
-
-/*
- *  Send response to the client
- * */
-function sendResponse (req, res, content, status, contentType, force) {
-
-    // if not provided, status is 200
-    status = status || 200;
-
-    // set the content type
-    contentType = contentType || "application/json";
-
-    // set headers and status code
-    res.writeHead(status, {"Content-Type": contentType});
-
-    // handle mongo errors
-    if (content instanceof Error) {
-        content = content.message;
-    }
-
-    // build response
-    var response = (content || {}).constructor === Object ? content : {};
-
-    // if content is string
-    if (typeof content === "string") {
-
-        // set output
-        response.output = content;
-
-        // but if it is an error
-        if (status !== 200) {
-
-            // set error
-            response.error = content;
-
-            // and delete output
-            delete response.output;
-        }
-    }
-
-    // if we force the response data
-    if (force) {
-        // force it!
-        res.end(content);
-        return;
-    }
-
-    // and end the response
-    res.end(JSON.stringify(response, null, 4));
-};
-
-// create http server
-var server = http.createServer(function(req, res) {
-
-
-    // serve files
-    ghc_server.serve (req, res);
-}).listen(9000);
 console.log("Listening on 9000");
 
 // Socket.io server listens to our app
-var io = require('socket.io').listen(server, {log: false});
+var io = SocketIO.listen(server._server, { log: false });
 
 // listen for connections
 io.sockets.on('connection', function (client) {
